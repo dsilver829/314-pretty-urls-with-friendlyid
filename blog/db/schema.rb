f044b3b2697b655b8a53472c3445d50ef683b633
David Silver <dsilver@candidatemetrics.com>
2012-10-02 07:20:08 -0700

    redirect appropriately

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121002141321) do
+ActiveRecord::Schema.define(:version => 20121002141728) do
 
   create_table "articles", :force => true do |t|
     t.string   "name"
@@ -23,5 +23,16 @@ ActiveRecord::Schema.define(:version => 20121002141321) do
   end
 
   add_index "articles", ["slug"], :name => "index_articles_on_slug"
+
+  create_table "friendly_id_slugs", :force => true do |t|
+    t.string   "slug",                         :null => false
+    t.integer  "sluggable_id",                 :null => false
+    t.string   "sluggable_type", :limit => 40
+    t.datetime "created_at"
+  end
+
+  add_index "friendly_id_slugs", ["slug", "sluggable_type"], :name => "index_friendly_id_slugs_on_slug_and_sluggable_type", :unique => true
+  add_index "friendly_id_slugs", ["sluggable_id"], :name => "index_friendly_id_slugs_on_sluggable_id"
+  add_index "friendly_id_slugs", ["sluggable_type"], :name => "index_friendly_id_slugs_on_sluggable_type"
 
 end
